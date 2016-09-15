04ad184b833d918516d45d42958b6ae2f027b381
Russell Parker <russell.parker7@gmail.com>
2016-09-15 00:55:02 -0400

    Format output, document getStatus function

--- tufts-meals.py
+++ tufts-meals.py
@@ -14,11 +14,11 @@ import sys
 #      skey (session key).
 #   2. GET intermediate "Please wait while we access your account." page.
 #      For some reason the login process fails if this is skipped.
-#   3. GET login-check.php page, parse out value of <message> tag which
+#   3. GET login-check.php xml page, parse out value of <message> tag which
 #      indicates whether or not the process is complete. Continue requesting
 #      the page after pausing for .3 seconds until the response indicates a
 #      successful login or 15 attempts have been made.
-#   4. GET final (index.php) page, parse JumboCash and meal swipe balances
+#   4. GET final (index.php) page, parse JumboCash and meal swipe balances.
 
 
 ##############################      PART 1      ################################
@@ -43,11 +43,11 @@ requests.get(URL + "/login.php", params=payload)
 
 ##############################      PART 3      ################################
 
-# getStatus: returns the server status on retrieving user data. Return codes:
-#            1: server is ready  -- user logged in
-#            0: server is not ready -- user not logged in
-#            -1: server error (as far as I can tell)
 def getStatus():
+    """Parse server status and return status code:
+       1:  server is ready 
+       0:  server is not ready
+       -1: server error"""
     res = requests.get(URL + "/login-check.php", params={'skey': skey})
     soup = bs4.BeautifulSoup(res.text, "html.parser")
     return int(soup.message.text)
@@ -63,12 +63,14 @@ while serverStatus != 1:
     if counter >= 15:
         sys.stderr.write("Checked login 15 times without success.")
         sys.exit()
-    time.sleep(.3)
+    time.sleep(0.3)
 
 
 ##############################      PART 4      ################################
 
 res = requests.get(URL + '/index.php', params={'skey': skey, 'cid': CID})
 soup = bs4.BeautifulSoup(res.text, "html.parser")
-for result in soup.findAll('tfoot'):
-    print result.th.findNext().text
+jumbocash, swipes = map(lambda t: t.th.findNext().text, soup.findAll('tfoot'))
+
+print 'Remaining Swipes: {}'.format(swipes)
+print 'JumboCash Balance: ${}'.format(jumbocash)
